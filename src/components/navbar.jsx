7dd39645dedb1d9d4c1588d014f301638170ddbc
Bouba <bboubacar366@gmail.com>
2023-10-16 17:15:49 +0200

    feat : add button for going to the comment

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,26 +5,24 @@ const Navbar = () => {
     const handleBack = () => {
         navigate("/");
     }
+        const handleRate = () => {
+        navigate("/#contact");
+    }
     return(
         <>
-        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <nav class="flex items-center justify-between h-auto w-full flex-wrap bg-teal-500 p-6">
             <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                <span class="font-semibold text-xl tracking-tight">VR CATAGLOGUE</span>
-            </div>
-            <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
+                <img src="img/Logo-e.png" className="w-16 h-16" />
+                <span class="font-semibold text-2xl tracking-tight">VR CATAGLOGUE</span>
             </div>
             <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div class="text-sm lg:flex-grow">
+                <div class="text-lg lg:flex-grow">
                 <a href="#" onClick={handleBack} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Back To Home Page
                 </a>
                 </div>
                 <div>
-                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+                <a href="#" onClick={handleRate} class="inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Rate Our Visit</a>
                 </div>
             </div>
         </nav>
--- src/components/unityProject.jsx
+++ src/components/unityProject.jsx
@@ -1,14 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "./navbar";
 import { Testimonials } from "./testimonials";
 
 const UnityProject = () => {
-
-
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/#contact")
+    }
     return(
-        <>
-            <Navbar />
-            <Testimonials />
-        </>
+        <div className="flex flex-col m-auto p-auto">
+            <div className="mb-12">
+                 <Navbar />
+                <Testimonials />
+            </div>
+        
+            <button onClick={handleClick} class="bg-transparent m-auto  hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                Leave us a comment
+            </button>
+        </div>
     )
 }
 
